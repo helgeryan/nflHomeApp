931cff9168d3068d5004a8abdc38fb6e28d1efb9
Ryan Helgeson <ryanhelgeson14@gmail.com>
2023-08-04 11:12:56 -0400

    Initial Commit

---
+++ nflStatApp/nflStatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  nflStatAppApp.swift
+//  nflStatApp
+//
+//  Created by Ryan Helgeson on 8/4/23.
+//
+
+import SwiftUI
+
+@main
+struct nflStatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
