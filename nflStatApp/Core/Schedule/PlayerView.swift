c17d9e1c9401ad72e70c5ba3f48ad92f63ecce01
Ryan Helgeson <ryanhelgeson14@gmail.com>
2023-08-07 12:06:22 -0400

    Add in  a passing stats view

--- nflStatApp/Core/Schedule/PlayerView.swift
+++ nflStatApp/Core/Schedule/PlayerView.swift
@@ -60,8 +60,95 @@ struct PlayerView: View {
                     .font(.title2)
                     .foregroundColor(.black)
             }
+            
+            if let seasons = model.player.seasons {
+                StatsView(seasons: seasons)
+            }
             Spacer()
         }
         .navigationBarBackButtonHidden(true)
+    }
+}
+
+struct StatsView: View {
+    var seasons: [Season]
+    
+    var atts: [String] = [
+        "Year",
+        "Att",
+        "Cmp",
+        "Cmp%",
+        "Yds",
+        "TDs",
+        "Long",
+        "Int",
+        "Long TD",
+        "Rat"
+    ]
+    
+    var body: some View {
+        ScrollView {
+            ScrollView(.horizontal) {
+                VStack(alignment: .leading, spacing: 0) {
+                    HStack {
+                        ForEach(atts, id: \.self, content: { att in
+                            Text(att)
+                                .frame(width: 80)
+                                .foregroundColor(.white)
+                        })
+                    }
+                    .background(Color.nfl)
+                    .padding(.bottom , 2)
+                    
+                    ForEach(seasons, id: \.id) { season in
+                        if let teams = season.teams, season.type == "REG" {
+                            PassingStatsView(teams: teams, year: season.year)
+                                .padding(.bottom ,2)
+                        }
+                    }
+                }
+                .background(Color(hex: "EEEEEE"))
+                .cornerRadius(12)
+                .padding()
+            }
+        }
+    }
+}
+
+struct PassingStatsView: View {
+    var teams: [TeamSeasonStats]
+    var year: Int
+    var body: some View {
+        ForEach(teams, id: \.id) { team in
+            if let passingStats = team.statistics.passing {
+                HStack {
+                    StatView(stat: year)
+                    StatView(stat: passingStats.attempts)
+                    StatView(stat: passingStats.completions)
+                    StatView(stat: passingStats.cmp_pct)
+                    StatView(stat: passingStats.yards)
+                    StatView(stat: passingStats.touchdowns)
+                    StatView(stat: passingStats.longest)
+                    StatView(stat: passingStats.interceptions)
+                    StatView(stat: passingStats.longest_touchdown)
+                    StatView(stat: passingStats.rating)
+                }
+            }
+        }
+    }
+}
+
+struct StatView: View {
+    var stat: Any
+    var body: some View {
+        if let stat = stat as? Double {
+            Text(String(format: "%0.1f", stat))
+                .frame(width: 80)
+        }
+        
+        if let stat = stat as? Int {
+            Text(String(format: "%d", stat))
+                .frame(width: 80)
+        }
     }
 }
